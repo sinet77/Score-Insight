b9bca3059988f130911d12c7690f85db4c7e31a4
Paweł Kozłowski <152084345+sinet77@users.noreply.github.com>
2025-05-28 13:12:01 +0000

    style: footer design added

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,36 @@
+// App.tsx
 import { useState } from "react";
+import { useOutletContext } from "react-router-dom";
 import CountriesList from "@components/Pages/MainPage/CountriesAndLeagues/CountriesAndLeagues";
 import { LeagueTeams } from "@components/Teams/LeagueTeams";
 import { GoToH2H } from "@components/Pages/MainPage/GoToH2H/GoToH2H";
-import h2hTeams from "./assets/h2hteams.jpeg";
-import h2hPlayers from "./assets/h2hplayers.jpg";
-import styles from "./App.module.scss";
-import { useNavigate } from "react-router-dom";
-import { routes } from "./routes";
 import BannerSlider from "@components/Pages/MainPage/Banner/Banner";
 import News from "@components/Pages/MainPage/News/News";
-import fifa_logo from "./assets/fifa_logo.png"
+import styles from "./App.module.scss";
+import h2hTeams from "./assets/h2hteams.jpeg";
+import h2hPlayers from "./assets/h2hplayers.jpg";
+import fifa_logo from "./assets/fifa_logo.png";
+import { useNavigate } from "react-router-dom";
+import { routes } from "./routes";
+
+type ContextType = {
+  scrollToLeagues: () => void;
+  setSelectedLeagueId: (id: number) => void;
+  selectedLeagueId: number;
+  leaguesRef: React.RefObject<HTMLDivElement>;
+};
 
 function App() {
-  const [selectedLeagueId, setSelectedLeagueId] = useState<number | null>(39);
+  const {
+    setSelectedLeagueId,
+    selectedLeagueId,
+    leaguesRef,
+  } = useOutletContext<ContextType>();
+
   const [selectedSeason, setSelectedSeason] = useState<string>("");
 
-  const handleSeasonSelect = (selectedSeason: string) => {
-    setSelectedSeason(selectedSeason);
+  const handleSeasonSelect = (season: string) => {
+    setSelectedSeason(season);
   };
 
   const navigate = useNavigate();
@@ -25,10 +39,15 @@ function App() {
     <div className={styles["main-container"]}>
       <BannerSlider />
       <News />
-      <button className={styles["fifa-ranking-button"]} onClick={() => navigate(routes.ranking)}>
+      <button
+        className={styles["fifa-ranking-button"]}
+        onClick={() => navigate(routes.ranking)}
+      >
         <span className={styles["button-first-text-part"]}>Check newest</span>
         <img src={fifa_logo} alt={"fifa logo"} className={styles["fifa_logo"]} />
-        <span className={styles["button-text"]}>FIFA Ranking</span></button>
+        <span className={styles["button-text"]}>FIFA Ranking</span>
+      </button>
+
       <div className={styles["go-to-container"]}>
         <GoToH2H
           image={h2hTeams}
@@ -41,7 +60,7 @@ function App() {
           where={() => navigate(routes.comparePlayers)}
         />
       </div>
-      <div className={styles["countries-container"]}>
+      <div ref={leaguesRef} className={styles["countries-container"]}>
         <div style={{ height: "600px" }}>
           <CountriesList
             onLeagueSelect={setSelectedLeagueId}
--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,16 +1,31 @@
+import { useRef, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../components/Navbar/Navbar";
 import { Footer } from "@components/Footer/Footer";
-import styles from "./Layout.module.scss"
+import styles from "./Layout.module.scss";
 
 export default function Layout() {
+  const [selectedLeagueId, setSelectedLeagueId] = useState<number>(39);
+  const leaguesRef = useRef<HTMLDivElement>(null);
+
+  const scrollToLeagues = () => {
+    leaguesRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <div className={styles["main"]}>
       <Navbar />
       <div className={styles["content"]}>
-        <Outlet />
+        <Outlet
+          context={{
+            scrollToLeagues,
+            setSelectedLeagueId,
+            leaguesRef,
+            selectedLeagueId,
+          }}
+        />
       </div>
-      <Footer /> 
+      <Footer scrollToLeagues={scrollToLeagues} setSelectedLeagueId={setSelectedLeagueId} />
     </div>
   );
 }
